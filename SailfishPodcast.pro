4712665925f2ee805b1138536e6c04751b836156
ivan tkachenko <ratijas@users.noreply.github.com>
2017-04-28 13:29:05 +0300

    tr: ru_RU

--- SailfishPodcast.pro
+++ SailfishPodcast.pro
@@ -34,7 +34,7 @@ CONFIG += sailfishapp_i18n
 # planning to localize your app, remember to comment out the
 # following TRANSLATIONS line. And also do not forget to
 # modify the localized app name in the the .desktop file.
-TRANSLATIONS += translations/SailfishPodcast-de.ts
+TRANSLATIONS += translations/SailfishPodcast-ru.ts
 
 DISTFILES += \
     qml/pages/StationPage.qml \
--- translations/SailfishPodcast-ru.ts
+++ translations/SailfishPodcast-ru.ts
@@ -1,71 +1,71 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="ru_RU" sourcelanguage="en">
 <context>
     <name>EpisodesListView</name>
     <message>
         <source>Error: could not fetch station data</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка: не возможно загрузить данные</translation>
     </message>
     <message>
         <source>Update</source>
-        <translation type="unfinished"></translation>
+        <translation>Обновить</translation>
     </message>
 </context>
 <context>
     <name>SearchPage</name>
     <message>
         <source>RSS url</source>
-        <translation type="unfinished"></translation>
+        <translation>Адрес RSS</translation>
     </message>
     <message>
         <source>Search iTunes Store</source>
-        <translation type="unfinished"></translation>
+        <translation>Поиск в iTunes Store</translation>
     </message>
     <message>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Добавить</translation>
     </message>
     <message>
         <source>Add by RSS url</source>
-        <translation type="unfinished"></translation>
+        <translation>Добавить по адресу RSS</translation>
     </message>
 </context>
 <context>
     <name>StationHeader</name>
     <message>
         <source>Subscribe</source>
-        <translation type="unfinished"></translation>
+        <translation>Подписаться</translation>
     </message>
     <message>
         <source>Unsubscribe</source>
-        <translation type="unfinished"></translation>
+        <translation>Отписаться</translation>
     </message>
 </context>
 <context>
     <name>StationListElement</name>
     <message>
         <source>Loading...</source>
-        <translation type="unfinished"></translation>
+        <translation>Загрузка…</translation>
     </message>
     <message>
         <source>Episodes: </source>
-        <translation type="unfinished"></translation>
+        <translation>Выпусков: </translation>
     </message>
 </context>
 <context>
     <name>SubscriptionsPage</name>
     <message>
         <source>Subscriptions</source>
-        <translation type="unfinished"></translation>
+        <translation>Подписки</translation>
     </message>
     <message>
         <source>Search / Add RSS</source>
-        <translation type="unfinished"></translation>
+        <translation>Поиск / Добавить RSS</translation>
     </message>
     <message>
         <source>Update</source>
-        <translation type="unfinished"></translation>
+        <translation>Обновить</translation>
     </message>
 </context>
 </TS>
