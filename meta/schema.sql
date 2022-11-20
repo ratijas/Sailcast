1a110302dbe36153bd4d0d2bf80132c402b310a7
ivan tkachenko <me@ratijas.tk>
2022-11-20 21:51:45 +0300

    Fix database schema
    It is only being used for documentary purposes here.

--- meta/schema.sql
+++ meta/schema.sql
@@ -1,41 +1,41 @@
-CREATE TABLE IF NOT EXIST subscription (
-	sb_station INT PRIMARY KEY NOT NULL,
-	sb_active INT NOT NULL,
-	sb_priority INT NOT NULL,
+CREATE TABLE IF NOT EXISTS subscription(
+	sb_station INTEGER PRIMARY KEY NOT NULL,
+	sb_active INTEGER NOT NULL,
+	sb_priority INTEGER NOT NULL,
 	CONSTRAINT priority_unique UNIQUE (sb_priority)
 );
 
-CREATE TABLE IF NOT EXIST station (
-	st_id INT PRIMARY KEY NOT NULL AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS station(
+	st_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	st_name TEXT NOT NULL,
 	st_description TEXT,
-	st_cover INT,
+	st_cover INTEGER,
 	st_feed_url TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXIST episode (
-	ep_id INT PRIMARY KEY NOT NULL AUTOINCREMENT,
-	ep_station INT NOT NULL,
+CREATE TABLE IF NOT EXISTS episode(
+	ep_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	ep_station INTEGER NOT NULL,
 	ep_title TEXT NOT NULL,
 	ep_description TEXT,
-	ep_duration INT NOT NULL,
+	ep_duration INTEGER NOT NULL,
 	ep_guid TEXT,
-	ep_cover INT,
+	ep_cover INTEGER,
 	ep_date TEXT NOT NULL,
 	ep_url TEXT NOT NULL,
-	ep_playback_position INT NOT NULL DEFAULT 0,
-	ep_playback_status INT NOT NULL DEFAULT 0
+	ep_playback_position INTEGER NOT NULL DEFAULT 0,
+	ep_playback_status INTEGER NOT NULL DEFAULT 0
 );
 
-CREATE TABLE IF NOT EXIST cover (
-	cv_id INT PRIMARY KEY NOT NULL AUTOINCREMENT
+CREATE TABLE IF NOT EXISTS cover(
+	cv_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
 );
 
-CREATE TABLE IF NOT EXIST cover_size (
-	cs_id INT PRIMARY KEY NOT NULL AUTOINCREMENT,
-	cs_cover INT NOT NULL,
-	cs_width INT NOT NULL,
-	cs_height INT NOT NULL,
+CREATE TABLE IF NOT EXISTS cover_size(
+	cs_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	cs_cover INTEGER NOT NULL,
+	cs_width INTEGER NOT NULL,
+	cs_height INTEGER NOT NULL,
 	CONSTRAINT unique_size UNIQUE (
 		cs_cover,
 		cs_width,
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXIST cover_size (
 	) ON CONFLICT REPLACE
 );
 
-CREATE TABLE IF NOT EXIST genres (
-	gn_genre_id INT NOT NULL,
-	gn_station INT NOT NULL
+CREATE TABLE IF NOT EXISTS genres(
+	gn_genre_id INTEGER NOT NULL,
+	gn_station INTEGER NOT NULL
 );
